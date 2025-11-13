e23397ae77d673890b22255f84884a5e6d6f16e9
Harshdeep Singh <91598143+harshsinghji@users.noreply.github.com>
2025-11-13 17:52:55 +0530

    Delete test_puppeteer.js

--- test_puppeteer.js
+++
@@ -1,9 +0,0 @@
-import puppeteer from "puppeteer";
-
-(async () => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto("https://example.com");
-  console.log("✅ Puppeteer launched and loaded example.com");
-  await browser.close();
-})();
